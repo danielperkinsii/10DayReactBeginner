d5608381430e4be0fb981a69bcd71054a2581f4f
Daniel Perkins II <daniel.perkins99@gmail.com>
2020-09-12 15:03:00 -0500

    created user data

--- 10-day-beginner/src/components/User.jsx
+++ 10-day-beginner/src/components/User.jsx
@@ -3,7 +3,15 @@ import React, { Component } from 'react'
 class User extends React.Component {
     render() {
         return (
-            <div><strong>User information Here</strong></div>  
+            <div><div className='user-deets'>
+            <img src={this.props.userInformation.avatar}/>
+            <h3><a href='https://twitter.com/danielperkinsii'>{this.props.userInformation.name}</a></h3>
+            <p><strong>Location: </strong>{this.props.userInformation.location}</p>
+            <p><strong>Favorite Food: </strong>{this.props.userInformation.foodType}</p>
+            <p><strong>Age: </strong>{this.props.userInformation.age}</p>
+            <p><strong>Likes: </strong>{this.props.userInformation.likes}</p>
+            <p><strong>Twitter: </strong><a href='https://twitter.com/danielperkinsii'>{this.props.userInformation.twitterHandle}</a></p>
+          </div></div>  
         )
     }
 }
